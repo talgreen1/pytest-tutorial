4985aa557e18772544b2043d0dff54c403e02e40
tal.green <tal.green@controlup.com>
2022-04-17 14:11:59 +0300

    Add config example

--- tests/test_environemtn.py
+++ tests/test_environemtn.py
@@ -1,6 +1,12 @@
+from pytest import mark
+
+
 def test_env_is_qa(app_config):
     assert app_config.base_url == "http://qa-env.com"
+    assert app_config.app_port == 80
 
 
+@mark.skip(reason="broken test")
 def test_env_is_dev(app_config):
     assert app_config.base_url == "http://dev-env.com"
+    assert app_config.app_port == 8080
