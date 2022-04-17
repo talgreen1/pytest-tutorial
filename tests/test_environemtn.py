c15e0bc45ede70b49164a325b0d2c5c4795ee4d4
tal.green <tal.green@controlup.com>
2022-04-17 11:33:16 +0300

    Add config example

--- tests/test_environemtn.py
+++ tests/test_environemtn.py
@@ -1,6 +1,6 @@
-def test_env_is_qa(env):
-    assert env == 'qa'
+def test_env_is_qa(app_config):
+    assert app_config.base_url == "http://qa-env.com"
 
 
-def test_env_is_dev(env):
-    assert env == 'dev'
+def test_env_is_dev(app_config):
+    assert app_config.base_url == "http://dev-env.com"
