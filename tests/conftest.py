6b20a6f071f814f9a5ab98d7e6c92425c9f90da0
tal.green <tal.green@controlup.com>
2022-04-18 10:58:59 +0300

    Add config example

---
+++ tests/conftest.py
@@ -0,0 +1,40 @@
+import os
+
+import pytest
+
+weekdays1 = ['mon', 'tue', 'wed']
+weekdays2 = ['fri', 'sat', 'sun']
+filename = "file.txt"
+
+
+def pytest_configure():
+    pytest.weekdays1 = ['mon', 'tue', 'wed']
+    pytest.weekdays2 = ['fri', 'sat', 'sun']
+
+
+@pytest.fixture
+def setup01():
+    print("\n In setup01: Starting")
+    wk = pytest.weekdays1.copy()
+    wk.append('thur')
+    yield wk
+    print("\n In setup01: After yield")
+
+
+@pytest.fixture
+def setup02():
+    print("\n In setup02: Starting")
+    wk2 = pytest.weekdays2.copy()
+    wk2.insert(0, 'thur')
+    yield wk2
+    print("\n In setup02: After yield")
+
+
+@pytest.fixture
+def setup03():
+    f = open(filename, 'w')
+    f.write("Hello world!")
+    f.close()
+    f = open(filename, 'r+')
+    yield f
+    os.remove(filename)
--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,47 +1,10 @@
 import pytest
-import os
-
-weekdays1 = ['mon', 'tue', 'wed']
-weekdays2 = ['fri', 'sat', 'sun']
-filename = "file.txt"
 
 
-@pytest.fixture
-def setup01():
-    print("\n In setup01: Starting")
-    weekdays1.append('thur')
-    yield weekdays1
-    print("\n In setup01: After yield")
-    weekdays1.pop()
+def test_1(setup01):
+    print(setup01)
 
 
-@pytest.fixture
-def setup02():
-    print("\n In setup02: Starting")
-    weekdays2.insert(0, 'thur')
-    yield weekdays2
-    print("\n In setup02: After yield")
-    weekdays1.pop()
-
-
-@pytest.fixture
-def setup03():
-    f = open(filename, 'w')
-    f.write("Hello world!")
-    f.close()
-    f = open(filename, 'r+')
-    yield f
-    os.remove(filename)
-
-
-def test_extend_list(setup01):
-    setup01.extend(weekdays2)
-    assert setup01 == ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun']
-
-
-def test_len(setup01, setup02):
-    assert len(weekdays1 + setup02) == len(setup01 + weekdays2)
-
-
-def test_testfile(setup03):
-    assert setup03.readline() == "Hello world!"
+def test_del_item(setup01):
+    del setup01[-1]
+    assert setup01 == pytest.weekdays1
