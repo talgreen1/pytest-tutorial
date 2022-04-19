eaadb49f711c90ef2b960990328403881010fef0
tal.green <tal.green@controlup.com>
2022-04-19 11:30:48 +0300

    Add config example

--- tests/test_get_config_data.py
+++ tests/test_get_config_data.py
@@ -1,5 +1,7 @@
-from utils.config_parser import *
+from utils.ConfigFileParser import ConfigFileParser
+
+config = ConfigFileParser('qa.ini')
 
 
 def test_get_gmail_url():
-    print(get_gmail_url())
+    print(config.get_gmail_url())
