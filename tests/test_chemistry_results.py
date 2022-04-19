dc67bf448d191d74a83a6e09c0c48da87696f179
tal.green <tal.green@controlup.com>
2022-04-19 12:02:21 +0300

    Add config example

--- tests/test_chemistry_results.py
+++ tests/test_chemistry_results.py
@@ -1,21 +1,22 @@
 import time
 
+
 # run tests in parallel: pytest -n4
 def test_result_1_completes_as_expected():
     time.sleep(5)
-    print('Result 1 has completed')
+    print('\n>>>Result 1 has completed\n')
 
 
 def test_result_2_completes_as_expected():
     time.sleep(5)
-    print('Result 2 has completed')
+    print('\n>>>Result 2 has completed\n')
 
 
 def test_result_3_completes_as_expected():
     time.sleep(5)
-    print('Result 3 has completed')
+    print('\n>>>Result 3 has completed\n')
 
 
 def test_result_4_completes_as_expected():
     time.sleep(5)
-    print('Result 4 has completed')
+    print('\n>>>Result 4 has completed\n')
