7fae5c3692a469461e26c0fc5c645e50fc565860
tal.green <tal.green@controlup.com>
2022-04-24 15:56:02 +0300

    Add classes example

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from user import User
 user1 = User('talg', 'talgreen1@yahoo.com', '1234', 'qa automation')
 user1.get_user_info()
 
-user1.say_hello()
+print(user1.get_num_of_users())
+
+user2 = User('talg', 'talgreen1@yahoo.com', '1234', 'qa automation')
+
+print(User.get_num_of_users())
--- user.py
+++ user.py
@@ -1,9 +1,12 @@
 class User:
+    num_of_users = 0
+
     def __init__(self, name, email, password, current_job_title):
         self.email = email
         self.name = name
         self.password = password
         self.current_job_title = current_job_title
+        User.num_of_users += 1
 
     def change_password(self, new_password):
         self.password = new_password
@@ -18,3 +21,6 @@ class User:
     def say_hello():
         print('Hello')
 
+    @staticmethod
+    def get_num_of_users():
+        return User.num_of_users
