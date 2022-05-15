071137b98a99474b7d98ecf63e3bdf0abc306064
tal.green <tal.green@controlup.com>
2022-05-15 09:00:32 +0300

    Add selenium sample

--- sel/testsSelenium.py
+++ sel/testsSelenium.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -35,5 +36,22 @@ def test_2():
         assert driver.find_element(By.NAME, "code").text == code
     except AssertionError:
         logging.error("Test failed")
+
+    driver.close()
+
+
+def test_3():
+    driver = webdriver.Firefox()
+    driver.get("http://www.controlup.com")
+    assert "Python" in driver.title
+    driver.find_element(By.XPATH, "/div[2]/div/p[1]/input[1]").send_keys("admin")
+    driver.find_element(By.XPATH, "/div[2]/div/p[1]/input[2]").send_keys("qwe45tg")
+    driver.find_element(By.XPATH, "/div[2]/div/p[1]/div").click()
+
+    driver.find_element(By.XPATH, "/div[4]/p[2]/div").click()
+    time.sleep(3)
+
+    nicknaem = driver.find_element(By.ID, "nick").text
+    nicknaem1 = get_nickname_from_server("admin")
 
     driver.close()
